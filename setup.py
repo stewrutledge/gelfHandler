2e4b26bccc7b29d576dc2b097c7dd63b38c7fefe
Stewart Rutledge <stew.rutledge@gmail.com>
2014-05-06 21:15:09 +0200

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gelfHandler',
+    version='0.1C',
+    packages=['gelfHandler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    author='Stewart Rutledge',
+    author_email='stew.rutledge@gmail.com',
+    url='https://github.com/stewrutledge/gelfHandler'
+)
