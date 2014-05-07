9acbf7a98463ce84e2614cd55785012fb218ded5
Stewart Rutledge <stewart.rutledge@kb.se>
2014-05-07 12:10:31 +0200

    Upped the version number and removed a comma

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='gelfHandler',
-    version='0.1C',
-    packages=['gelfHandler',],
+    version='0.1.1',
+    packages=['gelfHandler'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     author='Stewart Rutledge',
