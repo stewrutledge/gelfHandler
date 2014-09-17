2f0a66e68a140b8a1a33dbc01ceb069dd4a0a25c
Stewart Rutledge <stew.rutledge@gmail.com>
2014-09-17 07:55:13 +0200

    Added a short description

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='gelfHandler',
-    version='0.1.1',
+    version='0.1.2',
     packages=['gelfHandler'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Log handler that ships in gelf format',
     long_description=open('README.txt').read(),
     author='Stewart Rutledge',
     author_email='stew.rutledge@gmail.com',
