8a749c13ec51ae691db3d479f22ae97479a098f8
Stewart Rutledge <stewart.rutledge@kb.se>
2016-12-21 15:52:46 +0100

    Added keywords

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 
 setup(
     name='gelfHandler',
-    version='0.1.2',
+    version='1.0.0',
     packages=['gelfHandler'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='Log handler that ships in gelf format',
     long_description=open('README.txt').read(),
     author='Stewart Rutledge',
     author_email='stew.rutledge@gmail.com',
-    url='https://github.com/stewrutledge/gelfHandler'
+    url='https://github.com/stewrutledge/gelfHandler',
+    keywords=['gelf', 'graylog', 'loghandler', 'handler']
 )
